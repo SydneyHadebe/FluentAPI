de9b15649499579e9e5d92743a1b280282b1aa51
Sydney Hadebe <sydneyndlv25@gmail.com>
2019-04-26 12:31:03 +0200

    Add files via upload
    FluentApi

---
+++ FluentAPI/FluentAPI/PlutoContext.cs
@@ -0,0 +1,52 @@
+﻿using System.Data.Entity;
+
+namespace DataAnnotations
+{
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>()
+               .Property(a => a.Name)
+               .IsRequired()
+               .HasMaxLength(255);
+
+            modelBuilder.Entity<Course>()
+                .Property(a => a.Description)
+                .IsRequired()
+                .HasMaxLength(2000);
+
+            modelBuilder.Entity<Course>()
+                .HasRequired(a => a.Author)
+                .WithMany(a => a.Courses)
+                .HasForeignKey(a => a.AuthorId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Course>()
+                .HasMany(a => a.Tags)
+                .WithMany(a => a.Courses)
+                .Map(m => 
+                   {
+                       m.ToTable("CourseTags");
+                       m.MapLeftKey("CourseId");
+                       m.MapRightKey("TagId");
+                   });
+
+
+            modelBuilder.Entity<Course>()
+                .HasRequired(c => c.Cover)
+                .WithRequiredPrincipal(c => c.course);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
